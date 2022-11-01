b8ba3cf0c5921eb7aa5ecfc5d0b41124ba6c455b
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-10-31 22:40:28 -0500

    test despliegue

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "sqlite://test.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://test.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
