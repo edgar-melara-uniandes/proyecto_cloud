a8ed2c3eee0c67cc290524899c68245ab72d4150
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-11-21 21:26:38 -0500

    ajustes iniciales migracion a subscriber de pub/sub

--- services/web/tasks/music_task.py
+++ services/web/tasks/music_task.py
@@ -2,28 +2,39 @@ import os
 import uuid
 import tasks.util as Util
 import datetime
+import json
 import tasks.mail_task_sendgrid as SengridMail
 
-from celery import Celery
+#from celery import Celery
 import subprocess
 from posixpath import splitext
 from tasks.cloud_storage_client import CloudStorageClient
+from google.cloud import pubsub_v1
 from tasks.modelos import Task
 
 import tasks.db as db
 
-BACKEND_URL = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
-BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
-celery_app = Celery('music_conversions_batch', backend=BACKEND_URL, broker=BROKER_URL)
+#BACKEND_URL = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+#BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
+#celery_app = Celery('music_conversions_batch', backend=BACKEND_URL, broker=BROKER_URL)
 UPLOAD_FOLDER = str(os.environ.get("MEDIA_FOLDER", f"{os.getenv('APP_FOLDER')}/project/media"))
 ENDPOINT = str(os.environ.get("ENDPOINT_CONVERTED_DB", "http://127.0.0.1:1337/api/converted"))
 ENABLED_EMAIL = os.environ.get('ENABLED_EMAIL', 'false')
+GCP_PROJECT_NAME = os.environ.get("GCP_PROJECT_NAME", "nombre-proyecto")
+TARGET_SUBSCRIPTION = os.environ.get("TARGET_SUBSCRIPTION", "converter-topic-sub")
 
-@celery_app.task(name='music_conversions')
-def add_music_conversion_request(response):
-    print("Adding request to queue, musicID: " + str(response['userId']))
-    convert_audio_file(response)
-    print("After 60 seconds the request was attended")
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(GCP_PROJECT_NAME, TARGET_SUBSCRIPTION)
+
+#@celery_app.task(name='music_conversions')
+#def add_music_conversion_request(response):
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    #print("Adding request to queue, musicID: " + str(response['userId']))
+    decoded_message = json.loads(message)['userId']
+    print("Adding request to queue, musicID: " + str(decoded_message['userId']))
+    convert_audio_file(decoded_message)
+    #print("After 60 seconds the request was attended")
+    message.ack()
     return "Music converted :)"
 
 def convert_audio_file(response):
@@ -71,3 +82,17 @@ def updated_db(taskId, destination_blob_name):
     db.session.commit()
     return {"message": "Actualización realizada", "status": "success"}, 200
     
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}..\n")
+
+with subscriber:
+    try:
+        # When `timeout` is not set, result() will block indefinitely,
+        # unless an exception is encountered first.
+        # streaming_pull_future.result(timeout=timeout)
+        print("core")
+        streaming_pull_future.result(5)#normalmente NO se quiere valor aca, escucha indefinidamente 
+        print("coreend")
+    except TimeoutError:
+        streaming_pull_future.cancel()  # Trigger the shutdown.
+        streaming_pull_future.result()  # Block until the shutdown is complete.
